3adfa5d4361b4efdb6b9e32eae2ab714266e8066
Abiria <abiria.dev@gmail.com>
2023-09-26 17:27:33 +0900

    feat: add basic data model

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,1 +1,20 @@
-//
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct SearchResult {
+	pub total_count: u32,
+	pub incomplete_results: bool,
+	pub items: SearchResultItem,
+}
+
+#[derive(Deserialize)]
+pub struct SearchResultItem {
+	pub repository: SearchResultItemRepository,
+	pub html_url: String,
+	pub score: u32,
+}
+
+#[derive(Deserialize)]
+pub struct SearchResultItemRepository {
+	pub full_name: String,
+}
