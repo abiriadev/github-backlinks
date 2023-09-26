a80ab8d2a83482ca8592d32357a32426478dd5ba
Abiria <abiria.dev@gmail.com>
2023-09-26 18:19:56 +0900

    feat: implement basic resolver

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
+
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct SearchResult {
 	pub total_count: u32,
 	pub incomplete_results: bool,
-	pub items: SearchResultItem,
+	pub items: Vec<SearchResultItem>,
 }
 
 #[derive(Deserialize)]
@@ -17,4 +19,43 @@ pub struct SearchResultItem {
 #[derive(Deserialize)]
 pub struct SearchResultItemRepository {
 	pub full_name: String,
+}
+
+pub struct Backlink {
+	pub repository: String,
+	pub reference_count: u32,
+}
+
+pub trait Fetcher {
+	fn fetch_search_result(&mut self, repository_url: &str) -> SearchResult;
+}
+
+pub struct Resolver<T> {
+	url: String,
+	fetcher: T,
+}
+
+impl<T: Fetcher> Resolver<T> {
+	pub fn new(url: String, fetcher: T) -> Self { Self { url, fetcher } }
+
+	pub fn fetch_backlinks(&mut self) -> Vec<Backlink> {
+		let list = self
+			.fetcher
+			.fetch_search_result(&self.url);
+
+		let items = list.items;
+		let mut sb = HashMap::new();
+
+		for item in items {
+			*sb.entry(item.repository.full_name)
+				.or_default() += 1;
+		}
+
+		sb.into_iter()
+			.map(|(k, v)| Backlink {
+				repository: k,
+				reference_count: v,
+			})
+			.collect()
+	}
 }
